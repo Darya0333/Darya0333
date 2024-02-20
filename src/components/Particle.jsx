8ea9a5adebd203008b87846eaebdca76cb75734e
Alex Rio <ar02220604@gmail.com>
2024-02-21 01:06:16 +0900

    features: add-particle-system

---
+++ src/components/Particle.jsx
@@ -0,0 +1,90 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+const Particle = () => {
+    const particlesInit = async engine => {
+        await loadSlim(engine);
+    };
+
+    const particlesLoaded = async container => {
+    };
+    
+    return (
+        <div className="h-screen w-full bg-gradient-to-br from-[#6944ff] to-[#ff2846]">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push"
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4
+                            },
+                            repulse: {
+                                distance: 120,
+                                duration: 0.4
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff"
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1
+                        },
+                        collisions: {
+                            enable: true
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce"
+                            },
+                            random: false,
+                            speed: 1,
+                            straight: false
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: 0.5
+                        },
+                        shape: {
+                            type: "circle"
+                        },
+                        size: {
+                            value: { min: 1, max: 5 }
+                        }
+                    },
+                    detectRetina: true
+                }}
+            />
+        </div>
+    )
+}
+
+export default Particle;
