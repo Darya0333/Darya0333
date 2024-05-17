092389e65129530b1ffc74e012607c1d6cb953c2
foryou929 <zxcvwert3457@gmail.com>
2024-05-17 13:14:51 +0900

    feat: add project

--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@ function App() {
     { name: "UDATA株式会社", link: "https://udata.co.jp/", image: "/projects/Screenshot_4.png", content: "" },
     { name: "JILL STUART Beauty 公式サイト", link: "https://www.jillstuart-beauty.com/ja-jp/", image: "/projects/Screenshot_5.png", content: "" },
     { name: "Drug concentration simulation", link: "https://anesup.com", image: "/projects/Screenshot_6.png", content: "" },
+    { name: "Plant vs Zombies", link: "", image: "/projects/Screenshot_7.png", content: "" },
   ];
 
   const certifications = [
